91d4b11237ae368c17dc8c7482ab22701a956067
Luis I. Benavides <luis.i.benavides@accenture.com>
2015-02-15 12:43:22 -0500

    Implemented authentication using Spring Security storing credentials in application.yml.

---
+++ cams-ref-impl-angular-interface/src/main/java/com/accenture/cams/ri/ng/security/SecurityController.java
@@ -0,0 +1,15 @@
+package com.accenture.cams.ri.ng.security;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+}
