2c18537cecb4e17c7e6ef980ba6db511003315a1
Luis I. Benavides <luis.i.benavides@accenture.com>
2015-02-26 09:17:36 -0500

    Removed findAll method, since it is not required.

--- cams-ref-impl-angular-domain/src/main/java/com/accenture/cams/ri/ng/domain/repository/PlayerRepository.java
+++ cams-ref-impl-angular-domain/src/main/java/com/accenture/cams/ri/ng/domain/repository/PlayerRepository.java
@@ -1,11 +1,8 @@
 package com.accenture.cams.ri.ng.domain.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.accenture.cams.ri.ng.domain.entity.Player;
 
 public interface PlayerRepository extends JpaRepository<Player, String> {
-	Page<Player> findAll(Pageable pageable);
 }
