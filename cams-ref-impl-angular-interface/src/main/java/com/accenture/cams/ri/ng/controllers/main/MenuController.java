918f993c73a0f935eede499050e98a5471e5ad4f
Luis I. Benavides <luis.i.benavides@accenture.com>
2015-02-26 16:04:58 -0500

    Introduced value object to return to the client instead of persistent object. Note that we are using Dozer to map the entity object to the value object.

--- cams-ref-impl-angular-interface/src/main/java/com/accenture/cams/ri/ng/controllers/main/MenuController.java
+++ cams-ref-impl-angular-interface/src/main/java/com/accenture/cams/ri/ng/controllers/main/MenuController.java
@@ -3,6 +3,7 @@ package com.accenture.cams.ri.ng.controllers.main;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,20 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.accenture.cams.ri.ng.common.ApplicationMessage;
 import com.accenture.cams.ri.ng.common.RequestResult;
-import com.accenture.cams.ri.ng.domain.entity.HowToSystemUseCase;
 import com.accenture.cams.ri.ng.services.howToSystemUC.HowToSystemUCService;
+import com.accenture.cams.ri.ng.vos.HowToSystemUseCaseVO;
 
 @RestController
 @RequestMapping("/Menu")
 public class MenuController {
 
     @Autowired
+    Mapper mapper;
+
+    @Autowired
     HowToSystemUCService howToSystemUCService;
 
     @RequestMapping(value = "/howToSystemUCFindAll", method = RequestMethod.GET)
-    public RequestResult<List<HowToSystemUseCase>> howToSystemUCFindAll() {
-	List<HowToSystemUseCase> howToSystemUCFindAll = howToSystemUCService.howToSystemUCFindAll();
-	return new RequestResult<List<HowToSystemUseCase>>(howToSystemUCFindAll, new ArrayList<ApplicationMessage>());
+    public RequestResult<List<HowToSystemUseCaseVO>> howToSystemUCFindAll() {
+	List<HowToSystemUseCaseVO> howToSystemUCFindAllVO = new ArrayList<HowToSystemUseCaseVO>();
+	mapper.map(howToSystemUCService.howToSystemUCFindAll(), howToSystemUCFindAllVO);
+	return new RequestResult<List<HowToSystemUseCaseVO>>(howToSystemUCFindAllVO,
+		new ArrayList<ApplicationMessage>());
     }
-
 }
